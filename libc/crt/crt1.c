83ab4791a8326044ca3657d6089339a1a93da6ce
Alex <enderice2@users.noreply.github.com>
2022-04-26 21:58:34 +0300

    update crt1

--- libc/crt/crt1.c
+++ libc/crt/crt1.c
@@ -1,7 +1,25 @@
 long *stckptr;
 static char *argv[20];
-extern int main(int argc, char **argv, ...);
-void _start(void)
+extern int main(int argc, char **argv);
+
+__attribute__((naked)) static inline unsigned long crt1_syscall(long call, ...)
+{
+    __asm__ volatile("push %rbp\n"
+                     "mov %rsp, %rbp\n"
+                     "mov %rdi, %rax\n"
+                     "mov %rsi, %rdi\n"
+                     "mov %rdx, %rsi\n"
+                     "mov %rcx, %rdx\n"
+                     "mov %r8, %r10\n"
+                     "mov %r9, %r8\n"
+                     "mov 8(%rsp), %r9\n"
+                     "syscall\n"
+                     "mov %rbp, %rsp\n"
+                     "pop %rbp\n"
+                     "ret");
+}
+
+void _start()
 {
     __asm__("mov %%rsp, %%rcx"
             : "=c"(stckptr)
@@ -11,16 +29,11 @@ void _start(void)
     {
         for (int i = 0; i < argc; i++)
             argv[i] = ((char *)(stckptr + 2 + 8 * i));
-        main(argc, argv, (void *)0);
+        main(argc, argv);
     }
     else
-        main(0, (void *)0, (void *)0);
-    unsigned long ret;
-    __asm__ volatile("syscall"
-                     : "=a"(ret)
-                     : "a"(1), "D"(0)
-                     : "rcx", "r11", "memory");
-    if (ret == 0)
+        main(0, (void *)0);
+    if (crt1_syscall(1, 0) == 0)
         while (1)
             ;
 }
