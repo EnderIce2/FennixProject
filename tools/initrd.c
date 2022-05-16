c0638b38f75dc69d44d634f1199a1b9377ee67e5
Alex <enderice2@users.noreply.github.com>
2022-05-16 15:18:52 +0300

    oopsie

--- tools/initrd.c
+++ tools/initrd.c
@@ -37,13 +37,13 @@ int main(char argc, char **argv)
         headers[i].magic = 0xBF;
     }
     FILE *wstream = fopen("./initrd.img", "w");
-    unsigned char *data = (unsigned char *)kmalloc(off);
+    unsigned char *data = (unsigned char *)malloc(off);
     fwrite(&nheaders, sizeof(int), 1, wstream);
     fwrite(headers, sizeof(struct initrd_header), 64, wstream);
     for (i = 0; i < nheaders; i++)
     {
         FILE *stream = fopen(argv[i * 2 + 1], "r");
-        unsigned char *buf = (unsigned char *)kmalloc(headers[i].length);
+        unsigned char *buf = (unsigned char *)malloc(headers[i].length);
         fread(buf, 1, headers[i].length, stream);
         fwrite(buf, 1, headers[i].length, wstream);
         fclose(stream);
