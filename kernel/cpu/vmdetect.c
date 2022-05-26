90c673fd0afd3ecb00ad630a47e38a57ddf0c4a3
Alex <enderice2@users.noreply.github.com>
2022-05-26 20:43:42 +0300

    check first cpu vendor, then vmware backdoor and other stuff

--- kernel/cpu/vmdetect.c
+++ kernel/cpu/vmdetect.c
@@ -48,6 +48,72 @@ typedef struct
 
 bool CheckRunningUnderVM()
 {
+    // cpu vendor check
+    {
+        uint32_t rax, rbx, rcx, rdx;
+        char hyper_vendor_id[13];
+        cpuid(0x40000000, &rax, &rbx, &rcx, &rdx);
+        memcpy(hyper_vendor_id + 0, &rbx, 4);
+        memcpy(hyper_vendor_id + 4, &rcx, 4);
+        memcpy(hyper_vendor_id + 8, &rdx, 4);
+        hyper_vendor_id[12] = '\0';
+        debug("Vendor: %s", hyper_vendor_id);
+        if (!strcmp(hyper_vendor_id, CPUID_VENDOR_KVM))
+            return true;
+        if (!strcmp(hyper_vendor_id, CPUID_VENDOR_VMWARE))
+            return true;
+        if (!strcmp(hyper_vendor_id, CPUID_VENDOR_VIRTUALBOX))
+            return true;
+        if (!strcmp(hyper_vendor_id, CPUID_VENDOR_XENHVM))
+            return true;
+        if (!strcmp(hyper_vendor_id, CPUID_VENDOR_MICROSOFT_HV))
+            return true;
+        if (!strcmp(hyper_vendor_id, CPUID_VENDOR_PARALLELS))
+            return true;
+        debug("CPU vendor check failed");
+    }
+
+    // vmware backdoor check
+    {
+        VMwareCMD cmd;
+        cmd.bx = ~VMWARE_HYPERVISOR_MAGIC;
+        cmd.command = VMWARE_PORT_CMD_GETVERSION;
+        cmd.magic = VMWARE_HYPERVISOR_MAGIC;
+        cmd.port = VMWARE_HYPERVISOR_PORT;
+
+        asm volatile("in %%dx, %0"
+                     : "+a"(cmd.ax),
+                       "+b"(cmd.bx),
+                       "+c"(cmd.cx),
+                       "+d"(cmd.dx),
+                       "+S"(cmd.si),
+                       "+D"(cmd.di));
+
+        if (cmd.bx == VMWARE_HYPERVISOR_MAGIC || cmd.ax != 0xFFFFFFFF)
+            return true;
+        debug("VMWARE backdoor check failed");
+    }
+
+    // vmware hypervisor check
+    {
+        uint32_t rax, rbx, rcx, rdx;
+        VMWARE_PORT(GETVERSION, rax, rbx, rcx, rdx);
+        debug("rbx: %#llx", rbx);
+        if (rbx == VMWARE_HYPERVISOR_MAGIC)
+            return true;
+        debug("VMWARE hypervisor check failed");
+    }
+
+    // vmware dmi check
+    {
+        // char string[10];
+        // TODO: not implemented
+        // GetBIOSVendor(string);
+        // if (!memcmp(string, "VMware-", 7) || !memcmp(string, "VMW", 3))
+        //     return true;
+        // debug("DMI check failed");
+    }
+
     // check IDT
     {
         uint8_t m[6];
@@ -111,72 +177,6 @@ bool CheckRunningUnderVM()
 
         uint32_t rax, rbx, rcx, rdx;
         // TODO: check with vmread instruction
-    }
-
-    // vmware backdoor check
-    {
-        VMwareCMD cmd;
-        cmd.bx = ~VMWARE_HYPERVISOR_MAGIC;
-        cmd.command = VMWARE_PORT_CMD_GETVERSION;
-        cmd.magic = VMWARE_HYPERVISOR_MAGIC;
-        cmd.port = VMWARE_HYPERVISOR_PORT;
-
-        asm volatile("in %%dx, %0"
-                     : "+a"(cmd.ax),
-                       "+b"(cmd.bx),
-                       "+c"(cmd.cx),
-                       "+d"(cmd.dx),
-                       "+S"(cmd.si),
-                       "+D"(cmd.di));
-
-        if (cmd.bx == VMWARE_HYPERVISOR_MAGIC || cmd.ax != 0xFFFFFFFF)
-            return true;
-        debug("VMWARE backdoor check failed");
-    }
-
-    // vmware hypervisor check
-    {
-        uint32_t rax, rbx, rcx, rdx;
-        VMWARE_PORT(GETVERSION, rax, rbx, rcx, rdx);
-        debug("rbx: %#llx", rbx);
-        if (rbx == VMWARE_HYPERVISOR_MAGIC)
-            return true;
-        debug("VMWARE hypervisor check failed");
-    }
-
-    // vmware dmi check
-    {
-        // char string[10];
-        // TODO: not implemented
-        // GetBIOSVendor(string);
-        // if (!memcmp(string, "VMware-", 7) || !memcmp(string, "VMW", 3))
-        //     return true;
-        // debug("DMI check failed");
-    }
-
-    // cpu vendor check
-    {
-        uint32_t rax, rbx, rcx, rdx;
-        char hyper_vendor_id[13];
-        cpuid(0x40000000, &rax, &rbx, &rcx, &rdx);
-        memcpy(hyper_vendor_id + 0, &rbx, 4);
-        memcpy(hyper_vendor_id + 4, &rcx, 4);
-        memcpy(hyper_vendor_id + 8, &rdx, 4);
-        hyper_vendor_id[12] = '\0';
-        debug("Vendor: %s", hyper_vendor_id);
-        if (!strcmp(hyper_vendor_id, CPUID_VENDOR_KVM))
-            return true;
-        if (!strcmp(hyper_vendor_id, CPUID_VENDOR_VMWARE))
-            return true;
-        if (!strcmp(hyper_vendor_id, CPUID_VENDOR_VIRTUALBOX))
-            return true;
-        if (!strcmp(hyper_vendor_id, CPUID_VENDOR_XENHVM))
-            return true;
-        if (!strcmp(hyper_vendor_id, CPUID_VENDOR_MICROSOFT_HV))
-            return true;
-        if (!strcmp(hyper_vendor_id, CPUID_VENDOR_PARALLELS))
-            return true;
-        debug("CPU vendor check failed");
     }
 
     // TODO: Somehow VirtualBox is still not detected in some circumstances.
