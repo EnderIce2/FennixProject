e3930de364ed72ef91c51620f9e3658a2282314f
Alex <enderice2@users.noreply.github.com>
2022-05-26 03:59:17 +0300

    +syscalls debug messages

--- kernel/syscalls/SystemCalls.cpp
+++ kernel/syscalls/SystemCalls.cpp
@@ -15,6 +15,14 @@
 #include <task.h>
 #include <asm.h>
 
+// #define DEBUG_SYSCALLS 1
+
+#ifdef DEBUG_SYSCALLS
+#define syscldbg(m, ...) debug(m, ##__VA_ARGS__)
+#else
+#define syscldbg(m, ...)
+#endif
+
 typedef struct _SyscallsRegs
 {
     uint64_t r15, r14, r13, r12, r11, r10, r9, r8;
@@ -41,26 +49,77 @@ static uint64_t internal_exit(uint64_t code)
     return 0;
 }
 
-static uint64_t internal_createprocess(char *Path, uint64_t arg0, uint64_t arg1) { return SysCreateProcessFromFile(Path, arg0, arg1, User)->ID; }
-static TCB *internal_createthread(uint64_t rip, uint64_t arg0, uint64_t arg1) { return SysCreateThread(SysGetCurrentProcess(), rip, arg0, arg1); }
-static PCB *internal_getcurrentprocess() { return SysGetCurrentProcess(); }
-static TCB *internal_getcurrentthread() { return SysGetCurrentThread(); }
-static uint64_t internal_getcurrentprocessid() { return SysGetCurrentProcess()->ID; }
-static uint64_t internal_getcurrentthreadid() { return SysGetCurrentThread()->ID; }
-static int internal_getschedulemode() { return CurrentTaskingMode; }
+static uint64_t internal_createprocess(char *Path, uint64_t arg0, uint64_t arg1)
+{
+    syscldbg("syscall: createprocess( %s %#llx %#llx )", Path, arg0, arg1);
+    return SysCreateProcessFromFile(Path, arg0, arg1, User)->ID;
+}
 
-static Tasking::TaskControlBlock *internal_createtask(uint64_t rip, uint64_t arg0, uint64_t arg1, char *name) { Tasking::monot->CreateTask(rip, arg0, arg1, name, true); }
-static void internal_pushtask(uint64_t a, uint64_t b, uint64_t c, uint64_t d, SyscallsRegs *regs) { Tasking::monot->PushTask(RIP); }
-static void internal_poptask() { Tasking::monot->PopTask(); }
+static TCB *internal_createthread(uint64_t rip, uint64_t arg0, uint64_t arg1)
+{
+    syscldbg("syscall: createthread( %#llx %#llx %#llx )", rip, arg0, arg1);
+    return SysCreateThread(SysGetCurrentProcess(), rip, arg0, arg1);
+}
+
+static PCB *internal_getcurrentprocess()
+{
+    syscldbg("syscall: getcurrentprocess()");
+    return SysGetCurrentProcess();
+}
+
+static TCB *internal_getcurrentthread()
+{
+    syscldbg("syscall: getcurrentthread()");
+    return SysGetCurrentThread();
+}
+
+static uint64_t internal_getcurrentprocessid()
+{
+    syscldbg("syscall: getcurrentprocessid()");
+    return SysGetCurrentProcess()->ID;
+}
+
+static uint64_t internal_getcurrentthreadid()
+{
+    syscldbg("syscall: getcurrentthreadid()");
+    return SysGetCurrentThread()->ID;
+}
+
+static int internal_getschedulemode()
+{
+    syscldbg("syscall: getschedulemode()");
+    return CurrentTaskingMode;
+}
+
+static Tasking::TaskControlBlock *internal_createtask(uint64_t rip, uint64_t arg0, uint64_t arg1, char *name)
+{
+    syscldbg("syscall: createtask( %#llx %#llx %#llx %s )", rip, arg0, arg1, name);
+    return Tasking::monot->CreateTask(rip, arg0, arg1, name, true);
+}
+
+static void internal_pushtask(uint64_t a, uint64_t b, uint64_t c, uint64_t d, SyscallsRegs *regs)
+{
+    syscldbg("syscall: pushtask( %#llx %#llx %#llx %#llx )", a, b, c, d);
+    Tasking::monot->PushTask(RIP);
+}
+
+static void internal_poptask()
+{
+    syscldbg("syscall: poptask()");
+    Tasking::monot->PopTask();
+}
 
 static void *internal_requestpage()
 {
+    syscldbg("syscall: requestpage()");
     void *ret = KernelAllocator.RequestPage();
     KernelPageTableManager.MapMemory(ret, ret, PTFlag::US | PTFlag::RW);
     return ret;
 }
+
 static void internal_freepage(void *page)
 {
+    syscldbg("syscall: freepage( %p )", page);
     KernelAllocator.FreePage(page);
     KernelPageTableManager.UnmapMemory(page);
     KernelPageTableManager.MapMemory(page, page, PTFlag::RW);
@@ -68,13 +127,16 @@ static void internal_freepage(void *page)
 
 static void *internal_requestpages(uint64_t pages)
 {
+    syscldbg("syscall: requestpages( %#llx )", pages);
     void *ret = KernelAllocator.RequestPages(pages);
     for (uint64_t i = (uint64_t)ret; i < ((uint64_t)ret + (pages * PAGE_SIZE)); i += PAGE_SIZE)
         KernelPageTableManager.MapMemory((void *)i, (void *)i, PTFlag::US | PTFlag::RW);
     return ret;
 }
+
 static void internal_freepages(void *page, uint64_t pages)
 {
+    syscldbg("syscall: freepages( %p, %#llx )", page, pages);
     KernelAllocator.FreePages(page, pages);
     for (uint64_t i = (uint64_t)page; i < ((uint64_t)page + (pages * PAGE_SIZE)); i += PAGE_SIZE)
     {
@@ -83,30 +145,86 @@ static void internal_freepages(void *page, uint64_t pages)
     }
 }
 
-static uint64_t internal_fbaddress() { return CurrentDisplay->GetFramebuffer()->Address; }
-static uint64_t internal_fbsize() { return CurrentDisplay->GetFramebuffer()->Size; }
-static uint64_t internal_fbwidth() { return CurrentDisplay->GetFramebuffer()->Width; }
-static uint64_t internal_fbheight() { return CurrentDisplay->GetFramebuffer()->Height; }
-static uint64_t internal_fbppsl() { return CurrentDisplay->GetFramebuffer()->PixelsPerScanLine; }
+static uint64_t internal_fbaddress()
+{
+    syscldbg("syscall: fbaddress()");
+    return CurrentDisplay->GetFramebuffer()->Address;
+}
 
-static uint8_t internal_getlastkeyboardscancode() { return ps2keyboard->GetLastScanCode(); }
+static uint64_t internal_fbsize()
+{
+    syscldbg("syscall: fbsize()");
+    return CurrentDisplay->GetFramebuffer()->Size;
+}
 
-static FileSystem::FILE *internal_fileOpen(char *Path) { return vfs->Open(Path, nullptr); }
-static void internal_fileClose(FileSystem::FILE *File) { vfs->Close(File); }
-static uint64_t internal_fileRead(FileSystem::FILE *File, uint64_t Offset, void *Buffer, uint64_t Size) { return vfs->Read(File, Offset, Buffer, Size); }
-static uint64_t internal_fileWrite(FileSystem::FILE *File, uint64_t Offset, void *Buffer, uint64_t Size) { return vfs->Write(File, Offset, Buffer, Size); }
-static uint64_t internal_filesize(FileSystem::FILE *File) { return File->Node->Length; }
+static uint64_t internal_fbwidth()
+{
+    syscldbg("syscall: fbwidth()");
+    return CurrentDisplay->GetFramebuffer()->Width;
+}
 
-static void internal_usleep(uint64_t us) { usleep(us); }
+static uint64_t internal_fbheight()
+{
+    syscldbg("syscall: fbheight()");
+    return CurrentDisplay->GetFramebuffer()->Height;
+}
+
+static uint64_t internal_fbppsl()
+{
+    syscldbg("syscall: fbppsl()");
+    return CurrentDisplay->GetFramebuffer()->PixelsPerScanLine;
+}
+
+static uint8_t internal_getlastkeyboardscancode()
+{
+    syscldbg("syscall: getlastkeyboardscancode()");
+    return ps2keyboard->GetLastScanCode();
+}
+
+static FileSystem::FILE *internal_fileOpen(char *Path)
+{
+    syscldbg("syscall: fileOpen( %s )", Path);
+    return vfs->Open(Path, nullptr);
+}
+
+static void internal_fileClose(FileSystem::FILE *File)
+{
+    syscldbg("syscall: fileClose( %p )", File);
+    vfs->Close(File);
+}
+
+static uint64_t internal_fileRead(FileSystem::FILE *File, uint64_t Offset, void *Buffer, uint64_t Size)
+{
+    syscldbg("syscall: fileRead( %p, %#llx, %p, %#llx )", File, Offset, Buffer, Size);
+    return vfs->Read(File, Offset, Buffer, Size);
+}
+
+static uint64_t internal_fileWrite(FileSystem::FILE *File, uint64_t Offset, void *Buffer, uint64_t Size)
+{
+    syscldbg("syscall: fileWrite( %p, %#llx, %p, %#llx )", File, Offset, Buffer, Size);
+    return vfs->Write(File, Offset, Buffer, Size);
+}
+
+static uint64_t internal_filesize(FileSystem::FILE *File)
+{
+    syscldbg("syscall: filesize( %p )", File);
+    return File->Node->Length;
+}
+
+static void internal_usleep(uint64_t us)
+{
+    syscldbg("syscall: usleep( %#llx )", us);
+    usleep(us);
+}
 
 static uint64_t internal_dbg(int port, char *message)
 {
+    syscldbg("syscall: dbg( %d, %s )", port, message);
     serial_write_text(port, message);
     return 0;
 }
 
 static void *FennixSyscallsTable[] = {
-    // #ifndef __INTELLISENSE__ // hide "array designators are nonstandard in C/C++(2901)" error in vscode
     [_NullCall] = (void *)internal_unimpl,
     [_ProcessExit] = (void *)internal_exit,
     [_ProcessCreate] = (void *)internal_createprocess,
@@ -157,7 +275,6 @@ static void *FennixSyscallsTable[] = {
     [_usleep] = (void *)internal_usleep,
 
     [_DebugMessage] = (void *)internal_dbg,
-    // #endif
 };
 
 static void *LinuxSyscallsTable[] = {};
@@ -231,6 +348,7 @@ extern "C" uint64_t syscall_handler(SyscallsRegs *regs)
     {
     case Platform::UnknownPlatform:
         err("No platform is specified by the process? Guessing Native...");
+        [[fallthrough]];
     case Platform::Native:
     {
         uint64_t (*call)(uint64_t, ...) = reinterpret_cast<uint64_t (*)(uint64_t, ...)>(FennixSyscallsTable[RAX]);
@@ -246,10 +364,12 @@ extern "C" uint64_t syscall_handler(SyscallsRegs *regs)
     case Platform::Linux:
     {
         err("Platform Linux is not supported yet.");
+        [[fallthrough]];
     }
     case Platform::Windows:
     {
         err("Platform Windows is not supported yet.");
+        [[fallthrough]];
     }
     default:
         return -1;
