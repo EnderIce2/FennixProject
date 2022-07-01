dcbe19d5e7044a2d84bed255d56504244e389162
Alex <enderice2@users.noreply.github.com>
2022-07-01 07:38:20 +0300

    ? bad allocation fixed ?

--- kernel/Symbols.cpp
+++ kernel/Symbols.cpp
@@ -6,21 +6,23 @@
 
 #include "kernel.h"
 
+#include "test.h"
+
 #pragma GCC diagnostic ignored "-Wignored-qualifiers"
 
 KernelSymbols::Symbols *SymTbl = nullptr;
 
 namespace KernelSymbols
 {
-    Symbols::SymbolTable *SymTable;
+    Symbols::SymbolTable *SymTable = nullptr;
     uint64_t TotalEntries = 0;
 
     Symbols::Symbols()
     {
         Elf64_Ehdr *KernelHeader = (Elf64_Ehdr *)bootparams->kernel.file;
         Elf64_Shdr *ElfSections = (Elf64_Shdr *)(bootparams->kernel.file + KernelHeader->e_shoff);
-        Elf64_Sym *ElfSymbols;
-        char *strtab;
+        Elf64_Sym *ElfSymbols = nullptr;
+        char *strtab = nullptr;
 
         for (uint64_t i = 0; i < KernelHeader->e_shnum; i++)
             switch (ElfSections[i].sh_type)
@@ -43,40 +45,50 @@ namespace KernelSymbols
                 break;
             }
 
-        size_t Index, MinimumIndex;
-        for (size_t i = 0; i < TotalEntries - 1; i++)
+        if (ElfSymbols != nullptr && strtab != nullptr)
         {
-            MinimumIndex = i;
-            for (Index = i + 1; Index < TotalEntries; Index++)
-                if (ElfSymbols[Index].st_value < ElfSymbols[MinimumIndex].st_value)
-                    MinimumIndex = Index;
-            Elf64_Sym tmp = ElfSymbols[MinimumIndex];
-            ElfSymbols[MinimumIndex] = ElfSymbols[i];
-            ElfSymbols[i] = tmp;
-        }
+            size_t Index, MinimumIndex;
+            for (size_t i = 0; i < TotalEntries - 1; i++)
+            {
+                MinimumIndex = i;
+                for (Index = i + 1; Index < TotalEntries; Index++)
+                    if (ElfSymbols[Index].st_value < ElfSymbols[MinimumIndex].st_value)
+                        MinimumIndex = Index;
+                Elf64_Sym tmp = ElfSymbols[MinimumIndex];
+                ElfSymbols[MinimumIndex] = ElfSymbols[i];
+                ElfSymbols[i] = tmp;
+            }
 
-        while (ElfSymbols[0].st_value == 0)
-        {
-            ElfSymbols++;
-            TotalEntries--;
-        }
-        SymTable = new SymbolTable[TotalEntries];
-        for (size_t i = 0, g = TotalEntries; i < g; i++)
-        {
-            SymTable[i].Address = ElfSymbols[i].st_value;
-            SymTable[i].FunctionName = &strtab[ElfSymbols[i].st_name];
+            while (ElfSymbols[0].st_value == 0)
+            {
+                ElfSymbols++;
+                TotalEntries--;
+            }
+
+            trace("Symbol table loaded, %d entries (%ldKB)", TotalEntries, TO_KB(TotalEntries * sizeof(SymbolTable)));
+            // TODO: broken?
+            // SymTable = new SymbolTable[TotalEntries];
+            SymTable = (SymbolTable *)KernelAllocator.RequestPages((TotalEntries * sizeof(SymbolTable)) / PAGE_SIZE + 1);
+            // do_mem_test();
+
+            for (size_t i = 0, g = TotalEntries; i < g; i++)
+            {
+                SymTable[i].Address = ElfSymbols[i].st_value;
+                SymTable[i].FunctionName = &strtab[ElfSymbols[i].st_name];
+            }
         }
         BS->IncreaseProgres();
     }
 
     Symbols::~Symbols()
     {
-        delete SymTable;
+        // delete SymTable;
+        KernelAllocator.FreePages(SymTable, (TotalEntries * sizeof(SymbolTable)) / PAGE_SIZE + 1);
     }
 
     string Symbols::GetSymbolFromAddress(uint64_t Address)
     {
-        Symbols::SymbolTable Result{0, "<unknown>"};
+        Symbols::SymbolTable Result{0, (char *)"<unknown>"};
         for (size_t i = 0; i < TotalEntries; i++)
             if (SymTable[i].Address <= Address && SymTable[i].Address > Result.Address)
                 Result = SymTable[i];
