59f1e063c8994fa51722714e1bc865ca1d68de7f
Alex <enderice2@users.noreply.github.com>
2022-07-11 07:55:40 +0300

    Updated PCI descriptors

--- kernel/PCIDescriptors.cpp
+++ kernel/PCIDescriptors.cpp
@@ -27,7 +27,9 @@ namespace PCI
 
     enum PCIVendors
     {
+        SymbiosLogic = 0x1000,
         RedHat = 0x1AF4,
+        REDHat2 = 0x1B36,
         Realtek = 0x10EC,
         VirtualBox = 0x80EE,
         Ensoniq = 0x1274,
@@ -38,10 +40,216 @@ namespace PCI
         NVIDIACorporation = 0x10DE
     };
 
+    const char *MassStorageControllerSubclassName(uint8_t SubclassCode)
+    {
+        switch (SubclassCode)
+        {
+        case 0x00:
+            return "SCSI Bus Controller";
+        case 0x01:
+            return "IDE Controller";
+        case 0x02:
+            return "Floppy Disk Controller";
+        case 0x03:
+            return "IPI Bus Controller";
+        case 0x04:
+            return "RAID Controller";
+        case 0x05:
+            return "ATA Controller";
+        case 0x06:
+            return "Serial ATA";
+        case 0x07:
+            return "Serial Attached SCSI Controller";
+        case 0x08:
+            return "Non-Volatile Memory Controller";
+        case 0x80:
+            return "Mass Storage Controller";
+        }
+        fixme("Unknown mass storage controller %02x", SubclassCode);
+        return u8ToHexString(SubclassCode);
+    }
+
+    const char *NetworkControllerSubclassName(uint8_t SubclassCode)
+    {
+        switch (SubclassCode)
+        {
+        case 0x00:
+            return "Ethernet Controller";
+        case 0x01:
+            return "Token Ring Controller";
+        case 0x02:
+            return "FDDI Controller";
+        case 0x03:
+            return "ATM Controller";
+        case 0x04:
+            return "ISDN Controller";
+        case 0x05:
+            return "WorldFip Controller";
+        case 0x06:
+            return "PICMG HyperCard Controller";
+        case 0x07:
+            return "Infiniband Controller";
+        case 0x08:
+            return "Fabric Controller";
+        case 0x80:
+            return "Network Controller";
+        }
+        fixme("Unknown network controller %02x", SubclassCode);
+        return u8ToHexString(SubclassCode);
+    }
+
+    const char *DisplayControllerSubclassName(uint8_t SubclassCode)
+    {
+        switch (SubclassCode)
+        {
+        case 0x00:
+            return "VGA Compatible Controller";
+        case 0x01:
+            return "XGA Controller";
+        case 0x02:
+            return "3D Controller";
+        case 0x80:
+            return "Display Controller";
+        }
+        fixme("Unknown display controller %02x", SubclassCode);
+        return u8ToHexString(SubclassCode);
+    }
+
+    const char *CommunicationControllerSubclassName(uint8_t SubclassCode)
+    {
+        switch (SubclassCode)
+        {
+        case 0x00:
+            return "Serial Controller";
+        case 0x01:
+            return "Parallel Controller";
+        case 0x02:
+            return "Multi-Serial Controller";
+        case 0x03:
+            return "IEEE-1284 Controller";
+        case 0x04:
+            return "ATM Controller";
+        case 0x05:
+            return "Object Storage Controller";
+        case 0x80:
+            return "Communication controller";
+        }
+        fixme("Unknown communication controller %02x", SubclassCode);
+        return u8ToHexString(SubclassCode);
+    }
+
+    const char *BaseSystemPeripheralSubclassName(uint8_t SubclassCode)
+    {
+        // not sure if it's right
+        switch (SubclassCode)
+        {
+        case 0x00:
+            return "Unclassified";
+        case 0x01:
+            return "Keyboard";
+        case 0x02:
+            return "Pointing Device";
+        case 0x03:
+            return "Mouse";
+        case 0x04:
+            return "Scanner";
+        case 0x05:
+            return "Gameport";
+        case 0x80:
+            return "Unclassified";
+        }
+        fixme("Unknown base system peripheral %02x", SubclassCode);
+        return u8ToHexString(SubclassCode);
+    }
+
+    const char *SerialBusControllerSubclassName(uint8_t SubclassCode)
+    {
+        switch (SubclassCode)
+        {
+        case 0x00:
+            return "FireWire (IEEE 1394) Controller";
+        case 0x01:
+            return "ACCESS Bus Controller";
+        case 0x02:
+            return "SSA Controller";
+        case 0x03:
+            return "USB Controller";
+        case 0x04:
+            return "Fibre Channel Controller";
+        case 0x05:
+            return "SMBus Controller";
+        case 0x06:
+            return "Infiniband Controller";
+        case 0x07:
+            return "IPMI Interface Controller";
+        case 0x08:
+            return "SERCOS Interface (IEC 61491) Controller";
+        case 0x09:
+            return "CANbus Controller";
+        case 0x80:
+            return "Serial Bus Controller";
+        }
+        fixme("Unknown serial bus controller %02x", SubclassCode);
+        return u8ToHexString(SubclassCode);
+    }
+
+    const char *BridgeDeviceSubclassName(uint8_t SubclassCode)
+    {
+        switch (SubclassCode)
+        {
+        case 0x00:
+            return "Host Bridge";
+        case 0x01:
+            return "ISA Bridge";
+        case 0x02:
+            return "EISA Bridge";
+        case 0x03:
+            return "MCA Bridge";
+        case 0x04:
+            return "PCI-to-PCI Bridge";
+        case 0x05:
+            return "PCMCIA Bridge";
+        case 0x06:
+            return "NuBus Bridge";
+        case 0x07:
+            return "CardBus Bridge";
+        case 0x08:
+            return "RACEway Bridge";
+        case 0x09:
+            return "PCI-to-PCI Bridge";
+        case 0x0A:
+            return "InfiniBand-to-PCI Host Bridge";
+        case 0x80:
+            return "Bridge Device";
+        }
+        fixme("Unknown bridge device %02x", SubclassCode);
+        return u8ToHexString(SubclassCode);
+    }
+
+    const char *WirelessControllerSubclassName(uint8_t SubclassCode)
+    {
+        switch (SubclassCode)
+        {
+        case 0x11:
+            return "Bluetooth";
+        case 0x20:
+            return "802.1a controller";
+        case 0x21:
+            return "802.1b controller";
+        case 0x80:
+            return "Wireless controller";
+        }
+        fixme("Unknown wireless controller %02x", SubclassCode);
+        return u8ToHexString(SubclassCode);
+    }
+
     const char *GetVendorName(uint32_t VendorID)
     {
         switch (VendorID)
         {
+        case 0x1000:
+            return "Symbios Logic";
+        case 0x1B36:
         case 0x1AF4:
             return "Red Hat, Inc.";
         case 0x10EC:
@@ -75,6 +283,15 @@ namespace PCI
     {
         switch (VendorID)
         {
+        case SymbiosLogic:
+            switch (DeviceID)
+            {
+            case 0x30:
+                return "53c1030 PCI-X Fusion-MPT Dual Ultra320 SCSI";
+            case 0x1000:
+                return "63C815";
+            }
+            [[fallthrough]];
         case RedHat:
             switch (DeviceID)
             {
@@ -109,6 +326,43 @@ namespace PCI
             case 1110:
                 return "Inter-VM shared memory";
             case 0x1af41100:
+                return "QEMU Virtual Machine";
+            }
+            [[fallthrough]];
+        case REDHat2:
+            switch (DeviceID)
+            {
+            case 0x0001:
+                return "QEMU PCI-PCI bridge";
+            case 0x0002:
+                return "QEMU PCI 16550A Adapter";
+            case 0x0003:
+                return "QEMU PCI Dual-port 16550A Adapter";
+            case 0x0004:
+                return "QEMU PCI Quad-port 16550A Adapter";
+            case 0x0005:
+                return "QEMU PCI Test Device";
+            case 0x0006:
+                return "PCI Rocker Ethernet switch device";
+            case 0x0007:
+                return "PCI SD Card Host Controller Interface";
+            case 0x0008:
+                return "QEMU PCIe Host bridge";
+            case 0x0009:
+                return "QEMU PCI Expander bridge";
+            case 0x000A:
+                return "PCI-PCI bridge (multiseat)";
+            case 0x000B:
+                return "QEMU PCIe Expander bridge";
+            case 0x000C:
+                return "QEMU PCIe Root port";
+            case 0x000D:
+                return "QEMU XHCI Host Controller";
+            case 0x0010:
+                return "QEMU NVM Express Controller";
+            case 0x0100:
+                return "QXL paravirtual graphic card";
+            case 0x1AF41100:
                 return "QEMU Virtual Machine";
             }
             [[fallthrough]];
@@ -200,6 +454,8 @@ namespace PCI
                 return "82801GBM (ICH7-M) LPC Interface Bridge";
             case 0x07E0:
                 return "SATA AHCI controller";
+            case 0x293E:
+                return "82801I (ICH9 Family) HD Audio Controller";
             case 0x2935:
                 return "82801I (ICH9 Family) USB UHCI Controller #2";
             case 0x2936:
@@ -237,116 +493,52 @@ namespace PCI
         return u32ToHexString(DeviceID);
     }
 
-    const char *MassStorageControllerSubclassName(uint8_t SubclassCode)
-    {
-        switch (SubclassCode)
-        {
-        case 0x00:
-            return "SCSI Bus Controller";
-        case 0x01:
-            return "IDE Controller";
-        case 0x02:
-            return "Floppy Disk Controller";
-        case 0x03:
-            return "IPI Bus Controller";
-        case 0x04:
-            return "RAID Controller";
-        case 0x05:
-            return "ATA Controller";
-        case 0x06:
-            return "Serial ATA";
-        case 0x07:
-            return "Serial Attached SCSI";
-        case 0x08:
-            return "Non-Volatile Memory Controller";
-        case 0x80:
-            return "Other (msc)";
-        }
-        fixme("Unknown mass storage controller %02x", SubclassCode);
-        return u8ToHexString(SubclassCode);
-    }
-
-    const char *SerialBusControllerSubclassName(uint8_t SubclassCode)
-    {
-        switch (SubclassCode)
-        {
-        case 0x00:
-            return "FireWire (IEEE 1394) Controller";
-        case 0x01:
-            return "ACCESS Bus";
-        case 0x02:
-            return "SSA";
-        case 0x03:
-            return "USB Controller";
-        case 0x04:
-            return "Fibre Channel";
-        case 0x05:
-            return "SMBus";
-        case 0x06:
-            return "Infiniband";
-        case 0x07:
-            return "IPMI Interface";
-        case 0x08:
-            return "SERCOS Interface (IEC 61491)";
-        case 0x09:
-            return "CANbus";
-        case 0x80:
-            return "Other (sbc)";
-        }
-        fixme("Unknown serial bus controller %02x", SubclassCode);
-        return u8ToHexString(SubclassCode);
-    }
-
-    const char *BridgeDeviceSubclassName(uint8_t SubclassCode)
-    {
-        switch (SubclassCode)
-        {
-        case 0x00:
-            return "Host Bridge";
-        case 0x01:
-            return "ISA Bridge";
-        case 0x02:
-            return "EISA Bridge";
-        case 0x03:
-            return "MCA Bridge";
-        case 0x04:
-            return "PCI-to-PCI Bridge";
-        case 0x05:
-            return "PCMCIA Bridge";
-        case 0x06:
-            return "NuBus Bridge";
-        case 0x07:
-            return "CardBus Bridge";
-        case 0x08:
-            return "RACEway Bridge";
-        case 0x09:
-            return "PCI-to-PCI Bridge";
-        case 0x0a:
-            return "InfiniBand-to-PCI Host Bridge";
-        case 0x80:
-            return "Other (bds)";
-        }
-        fixme("Unknown bridge device %02x", SubclassCode);
-        return u8ToHexString(SubclassCode);
-    }
-
     const char *GetSubclassName(uint8_t ClassCode, uint8_t SubclassCode)
     {
         switch (ClassCode)
         {
+        case 0x00:
+            return "Unclassified";
         case 0x01:
             return MassStorageControllerSubclassName(SubclassCode);
+        case 0x02:
+            return NetworkControllerSubclassName(SubclassCode);
         case 0x03:
-            switch (SubclassCode)
-            {
-            case 0x00:
-                return "VGA Compatible Controller";
-            }
-            [[fallthrough]];
+            return DisplayControllerSubclassName(SubclassCode);
+        case 0x04:
+            return "Multimedia controller";
+        case 0x05:
+            return "Memory Controller";
         case 0x06:
             return BridgeDeviceSubclassName(SubclassCode);
+        case 0x07:
+            return CommunicationControllerSubclassName(SubclassCode);
+        case 0x08:
+            return BaseSystemPeripheralSubclassName(SubclassCode);
+        case 0x09:
+            return "Input device controller";
+        case 0x0A:
+            return "Docking station";
+        case 0x0B:
+            return "Processor";
         case 0x0C:
             return SerialBusControllerSubclassName(SubclassCode);
+        case 0x0D:
+            return WirelessControllerSubclassName(SubclassCode);
+        case 0x0E:
+            return "Intelligent controller";
+        case 0x0F:
+            return "Satellite communication controller";
+        case 0x10:
+            return "Encryption controller";
+        case 0x11:
+            return "Signal processing accelerators";
+        case 0x12:
+            return "Processing accelerators";
+        case 0x13:
+            return "Non-Essential Instrumentation";
+        case 0x40:
+            return "Coprocessor";
         }
         fixme("Unknown subclass name %02x:%02x", ClassCode, SubclassCode);
         return u8ToHexString(SubclassCode);
@@ -357,21 +549,39 @@ namespace PCI
         switch (ClassCode)
         {
         case 0x01:
+        {
             switch (SubclassCode)
             {
             case 0x06:
+            {
                 switch (ProgIF)
                 {
-                case 0x00:
-                    return "Vendor Specific Interface";
-                case 0x01:
-                    return "AHCI 1.0";
-                case 0x02:
-                    return "Serial Storage Bus";
+                case 0:
+                    return "Vendor Specific SATA Controller";
+                case 1:
+                    return "AHCI SATA Controller";
+                case 2:
+                    return "Serial Storage Bus SATA Controller";
+                default:
+                    return "SATA controller";
                 }
             }
-            [[fallthrough]];
+            case 0x08:
+            {
+                switch (ProgIF)
+                {
+                case 0x01:
+                    return "NVMHCI Controller";
+                case 0x02:
+                    return "NVM Express Controller";
+                default:
+                    return "Non-Volatile Memory Controller";
+                }
+            }
+            }
+        }
         case 0x03:
+        {
             switch (SubclassCode)
             {
             case 0x00:
@@ -381,19 +591,58 @@ namespace PCI
                     return "VGA Controller";
                 case 0x01:
                     return "8514-Compatible Controller";
+                default:
+                    return "VGA Compatible Controller";
                 }
             }
-            [[fallthrough]];
-        case 0x0C:
+        }
+        case 0x07:
+        {
             switch (SubclassCode)
             {
+            case 0x00:
+            {
+                switch (ProgIF)
+                {
+                case 0x00:
+                    return "Serial controller <8250>";
+                case 0x01:
+                    return "Serial controller <16450>";
+                case 0x02:
+                    return "Serial controller <16550>";
+                case 0x03:
+                    return "Serial controller <16650>";
+                case 0x04:
+                    return "Serial controller <16750>";
+                case 0x05:
+                    return "Serial controller <16850>";
+                case 0x06:
+                    return "Serial controller <16950";
+                default:
+                    return "Serial controller";
+                }
+            }
+            }
+        }
+        case 0x0C:
+        {
+            switch (SubclassCode)
+            {
+            case 0x00:
+                switch (ProgIF)
+                {
+                case 0x00:
+                    return "Generic FireWire (IEEE 1394) Controller";
+                case 0x10:
+                    return "OHCI FireWire (IEEE 1394) Controller";
+                }
             case 0x03:
                 switch (ProgIF)
                 {
                 case 0x00:
-                    return "UHCI Controller";
+                    return "UHCI (USB1) Controller";
                 case 0x10:
-                    return "OHCI Controller";
+                    return "OHCI (USB1) Controller";
                 case 0x20:
                     return "EHCI (USB2) Controller";
                 case 0x30:
@@ -401,11 +650,13 @@ namespace PCI
                 case 0x80:
                     return "Unspecified";
                 case 0xFE:
-                    return "USB Device (Not a Host Controller)";
+                    return "USB Device";
                 }
             }
         }
-        fixme("Unknown prog IF name %02x:%02x:%02x", ClassCode, SubclassCode, ProgIF);
+        }
+        // not really a fixme
+        // fixme("Unknown prog IF name %02x:%02x:%02x", ClassCode, SubclassCode, ProgIF);
         return u8ToHexString(ProgIF);
     }
 }
