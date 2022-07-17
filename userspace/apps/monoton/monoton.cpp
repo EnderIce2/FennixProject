2580ede448d9bb2927dbd9980b10986d8793bd48
Alex <enderice2@users.noreply.github.com>
2022-07-17 19:10:54 +0300

    declare display height syscall as variable

--- userspace/apps/monoton/monoton.cpp
+++ userspace/apps/monoton/monoton.cpp
@@ -48,9 +48,9 @@ int main(int argc, char **argv)
 
     uint64_t address = syscall_displayAddress();
     // uint64_t width = syscall_displayWidth();
-    // uint64_t height = syscall_displayHeight();
+    uint64_t height = syscall_displayHeight();
     uint64_t ppsl = syscall_displayPixelsPerScanLine();
-    for (int VerticalScanline = 0; VerticalScanline < syscall_displayHeight(); VerticalScanline++)
+    for (int VerticalScanline = 0; VerticalScanline < height; VerticalScanline++)
     {
         uint64_t PixelPtrBase = address + ((ppsl * 4) * VerticalScanline);
         for (uint32_t *PixelPtr = (uint32_t *)PixelPtrBase; PixelPtr < (uint32_t *)(PixelPtrBase + (ppsl * 4)); PixelPtr++)
