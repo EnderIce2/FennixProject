a8f7666980737318de1d0e85fae0efc70f94606a
Alex <enderice2@users.noreply.github.com>
2022-08-04 03:01:38 +0300

    CRITICAL: E1000 interrupt handler should be fixed asap

--- kernel/network/NetworkController.cpp
+++ kernel/network/NetworkController.cpp
@@ -7,15 +7,86 @@
 #include "../drivers/network/E1000/E1000.hpp"
 #include "../pci.h"
 
+// TODO: warning: deleting object of polymorphic class type ‘X::NetworkInterfaceController’ which has non-virtual destructor might cause undefined behavior
+
 NetworkInterfaceManager::NetworkInterface *nimgr = nullptr;
 
-RTL8139::NetworkInterfaceController *rtl8139 = nullptr;
-RTL8169::NetworkInterfaceController *rtl8169 = nullptr;
-E1000::NetworkInterfaceController *e1000 = nullptr;
+RTL8139::NetworkInterfaceController *rtl8139[8];
+int rtl8139Count = 0;
+
+RTL8169::NetworkInterfaceController *rtl8169[8];
+int rtl8169Count = 0;
+
+E1000::NetworkInterfaceController *e1000[8];
+int e1000Count = 0;
 
 InterruptHandler(E1000StubInterruptHandler)
 {
-    e1000->E1000InterruptHandler(regs);
+    // TODO: support more
+    e1000[0]->E1000InterruptHandler(regs);
+}
+
+void TraceCards()
+{
+    int rtl8139CardCount = 0;
+    foreach (auto card in rtl8139)
+    {
+        if (card)
+        {
+            MediaAccessControl mac = card->GetMAC();
+            if (!ValidMAC(mac))
+            {
+                err("RTL-8139[%d]: MAC address is invalid %x:%x:%x:%x:%x:%x", rtl8139CardCount,
+                    mac.Address[0], mac.Address[1], mac.Address[2], mac.Address[3], mac.Address[4], mac.Address[5]);
+            }
+            else
+                netdbg("RTL-8139[%d]: MAC: %02x:%02x:%02x:%02x:%02x:%02x", rtl8139CardCount,
+                       mac.Address[0], mac.Address[1], mac.Address[2], mac.Address[3], mac.Address[4], mac.Address[5]);
+        }
+        else
+            err("RTL-8139[%d]: Card not found", rtl8139CardCount);
+        rtl8139CardCount++;
+    }
+
+    int rtl8169CardCount = 0;
+    foreach (auto card in rtl8169)
+    {
+        if (card)
+        {
+            MediaAccessControl mac = card->GetMAC();
+            if (!ValidMAC(mac))
+            {
+                err("RTL-8169[%d]: MAC address is invalid %x:%x:%x:%x:%x:%x", rtl8169CardCount,
+                    mac.Address[0], mac.Address[1], mac.Address[2], mac.Address[3], mac.Address[4], mac.Address[5]);
+            }
+            else
+                netdbg("RTL-8169[%d]: MAC: %02x:%02x:%02x:%02x:%02x:%02x", rtl8169CardCount,
+                       mac.Address[0], mac.Address[1], mac.Address[2], mac.Address[3], mac.Address[4], mac.Address[5]);
+        }
+        else
+            err("RTL-8169[%d]: Card not found", rtl8169CardCount);
+        rtl8169CardCount++;
+    }
+
+    int e1000CardCount = 0;
+    foreach (auto card in e1000)
+    {
+        if (card)
+        {
+            MediaAccessControl mac = card->GetMAC();
+            if (!ValidMAC(mac))
+            {
+                err("E1000[%d]: MAC address is invalid %x:%x:%x:%x:%x:%x", e1000CardCount,
+                    mac.Address[0], mac.Address[1], mac.Address[2], mac.Address[3], mac.Address[4], mac.Address[5]);
+            }
+            else
+                netdbg("E1000[%d]: MAC: %02x:%02x:%02x:%02x:%02x:%02x", e1000CardCount,
+                       mac.Address[0], mac.Address[1], mac.Address[2], mac.Address[3], mac.Address[4], mac.Address[5]);
+        }
+        else
+            err("E1000[%d]: Card not found", e1000CardCount);
+        e1000CardCount++;
+    }
 }
 
 namespace NetworkInterfaceManager
@@ -32,89 +103,36 @@ namespace NetworkInterfaceManager
         // "AMD PCNET", 0x1022, 0x2000
 
         // TODO: initialize more cards at once
-        if (!rtl8139)
-            foreach (auto PCIData in PCI::FindPCIDevice(0x10EC, 0x8139))
-                rtl8139 = new RTL8139::NetworkInterfaceController(PCIData, CardIDs++);
+        foreach (auto PCIData in PCI::FindPCIDevice(0x10EC, 0x8139))
+            rtl8139[rtl8139Count++] = new RTL8139::NetworkInterfaceController(PCIData, CardIDs++);
 
-        if (!rtl8169)
-            foreach (auto PCIData in PCI::FindPCIDevice(0x10EC, 0x8169))
-                rtl8169 = new RTL8169::NetworkInterfaceController(PCIData, CardIDs++);
+        foreach (auto PCIData in PCI::FindPCIDevice(0x10EC, 0x8169))
+            rtl8169[rtl8169Count++] = new RTL8169::NetworkInterfaceController(PCIData, CardIDs++);
 
-        if (!e1000)
-            foreach (auto PCIData in PCI::FindPCIDevice(0x8086, 0x100E))
-            {
-                e1000 = new E1000::NetworkInterfaceController(PCIData, CardIDs++);
-                if (!ValidMAC(e1000->GetMAC()))
-                {
-                    delete e1000;
-                    err("E1000: Invalid MAC address!");
-                }
-                break;
-            }
+        foreach (auto PCIData in PCI::FindPCIDevice(0x8086, 0x100E))
+            e1000[e1000Count++] = new E1000::NetworkInterfaceController(PCIData, CardIDs++);
 
-        if (!e1000)
-            foreach (auto PCIData in PCI::FindPCIDevice(0x8086, 0x153A))
-            {
-                e1000 = new E1000::NetworkInterfaceController(PCIData, CardIDs++);
-                if (!ValidMAC(e1000->GetMAC()))
-                {
-                    delete e1000;
-                    err("E1000: Invalid MAC address!");
-                }
-                break;
-            }
+        foreach (auto PCIData in PCI::FindPCIDevice(0x8086, 0x153A))
+            e1000[e1000Count++] = new E1000::NetworkInterfaceController(PCIData, CardIDs++);
 
-        if (!e1000)
-            foreach (auto PCIData in PCI::FindPCIDevice(0x8086, 0x10EA))
-            {
-                e1000 = new E1000::NetworkInterfaceController(PCIData, CardIDs++);
-                if (!ValidMAC(e1000->GetMAC()))
-                {
-                    delete e1000;
-                    err("E1000: Invalid MAC address!");
-                }
-                break;
-            }
+        foreach (auto PCIData in PCI::FindPCIDevice(0x8086, 0x10EA))
+            e1000[e1000Count++] = new E1000::NetworkInterfaceController(PCIData, CardIDs++);
 
-        if (!e1000)
-            foreach (auto PCIData in PCI::FindPCIDevice(0x8086, 0x109A))
-            {
-                e1000 = new E1000::NetworkInterfaceController(PCIData, CardIDs++);
-                if (!ValidMAC(e1000->GetMAC()))
-                {
-                    delete e1000;
-                    err("E1000: Invalid MAC address!");
-                }
-                break;
-            }
+        foreach (auto PCIData in PCI::FindPCIDevice(0x8086, 0x109A))
+            e1000[e1000Count++] = new E1000::NetworkInterfaceController(PCIData, CardIDs++);
 
-        if (!e1000)
-            foreach (auto PCIData in PCI::FindPCIDevice(0x8086, 0x100F))
-            {
-                e1000 = new E1000::NetworkInterfaceController(PCIData, CardIDs++);
-                if (!ValidMAC(e1000->GetMAC()))
-                {
-                    delete e1000;
-                    err("E1000: Invalid MAC address!");
-                }
-                break;
-            }
+        foreach (auto PCIData in PCI::FindPCIDevice(0x8086, 0x100F))
+            e1000[e1000Count++] = new E1000::NetworkInterfaceController(PCIData, CardIDs++);
 
-        if (e1000)
+        if (e1000[0])
         {
-            RegisterInterrupt(E1000StubInterruptHandler, IRQ11, true);
-            e1000->Start();
-            MediaAccessControl mac = e1000->GetMAC();
-            if (!ValidMAC(mac))
-            {
-                err("E1000: MAC address is invalid %x:%x:%x:%x:%x:%x",
-                    mac.Address[0], mac.Address[1], mac.Address[2], mac.Address[3], mac.Address[4], mac.Address[5]);
-                delete e1000;
-            }
-            else
-                netdbg("E1000: MAC: %02x:%02x:%02x:%02x:%02x:%02x",
-                       mac.Address[0], mac.Address[1], mac.Address[2], mac.Address[3], mac.Address[4], mac.Address[5]);
+            /* FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME */
+            RegisterInterrupt(E1000StubInterruptHandler, IRQ10, true); // TODO: ASSIGN INTERRUPT PROPERLY!
+            RegisterInterrupt(E1000StubInterruptHandler, IRQ11, true); // TODO: ASSIGN INTERRUPT PROPERLY!
+            e1000[0]->Start();
         }
+
+        TraceCards();
     }
 
     NetworkInterface::~NetworkInterface()
