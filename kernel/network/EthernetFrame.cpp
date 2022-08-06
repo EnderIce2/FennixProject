c1c1c73db9b6919c5e5e032ed0fde3915a54cee1
Alex <enderice2@users.noreply.github.com>
2022-08-06 21:23:45 +0300

    Fixed wrong mac given

--- kernel/network/EthernetFrame.cpp
+++ kernel/network/EthernetFrame.cpp
@@ -16,9 +16,8 @@ namespace NetworkEthernetFrame
     void EthernetFrame::Send(MediaAccessControl MAC, FrameType Type, void *Data, uint64_t Length)
     {
         void *Packet = KernelAllocator.RequestPages(sizeof(EthernetFrameHeader) / PAGE_SIZE + 1);
-        /* Not sure if this will convert it correctly */
-        ((EthernetFrameHeader *)Packet)->SourceMAC = this->Interface->MAC.raw;
-        ((EthernetFrameHeader *)Packet)->DestinationMAC = MAC.raw;
+        ((EthernetFrameHeader *)Packet)->SourceMAC = MacToHex(this->Interface->MAC);
+        ((EthernetFrameHeader *)Packet)->DestinationMAC = MacToHex(MAC);
         ((EthernetFrameHeader *)Packet)->Type = Type;
 
         memcpy((void *)((uint64_t)Packet + sizeof(EthernetFrameHeader)), Data, Length);
@@ -28,5 +27,11 @@ namespace NetworkEthernetFrame
 
     void EthernetFrame::Receive(void *Data, uint64_t Length)
     {
+        EthernetFrameHeader *eFrame = (EthernetFrameHeader *)Data;
+
+        if (eFrame->DestinationMAC == 0xFFFFFFFFFFFF || eFrame->DestinationMAC == MacToHex(this->Interface->MAC))
+        {
+            fixme("EthernetFrame::Receive()");
+        }
     }
 }
