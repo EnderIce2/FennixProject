e7581abdce77ba672d4b24f67399dc76870aef3f
Alex <enderice2@users.noreply.github.com>
2022-08-12 22:57:35 +0300

    Ethernet fully implemented

--- kernel/network/Ethernet.cpp
+++ kernel/network/Ethernet.cpp
@@ -5,59 +5,113 @@
 
 namespace NetworkEthernet
 {
-    Ethernet::Ethernet(NetworkInterfaceManager::DeviceInterface *Interface) { this->Interface = Interface; }
-
+    Ethernet::Ethernet(NetworkInterfaceManager::DeviceInterface *Interface) : NetworkInterfaceManager::Events(Interface) { this->Interface = Interface; }
     Ethernet::~Ethernet() {}
 
-    void Ethernet::Send(void *Data, uint64_t Length, SendInfo Info)
+    void Ethernet::Send(MediaAccessControl MAC, FrameType Type, uint8_t *Data, uint64_t Length)
     {
-        uint64_t PacketLength = sizeof(EthernetPacket) + Length;
+        netdbg("ETH: Sending frame type %#x to %02x:%02x:%02x:%02x:%02x:%02x", Type,
+               MAC.Address[0], MAC.Address[1], MAC.Address[2],
+               MAC.Address[3], MAC.Address[4], MAC.Address[5]);
+        uint64_t PacketLength = sizeof(EthernetHeader) + Length;
         EthernetPacket *Packet = (EthernetPacket *)kmalloc(PacketLength);
-        Packet->Header = {
-            .DestinationMAC = Info.MAC,
-            .SourceMAC = Interface->MAC,
-            .Type = Info.Type,
-        };
+
+        Packet->Header.DestinationMAC = b48(MAC.ToHex());
+        Packet->Header.SourceMAC = b48(this->Interface->MAC.ToHex());
+        Packet->Header.Type = b16(Type);
+
         memcpy(Packet->Data, Data, Length);
-        Interface->Send((void *)Packet, PacketLength);
+        nimgr->Send(Interface, (uint8_t *)Packet, PacketLength);
         kfree(Packet);
-        netdbg("ETH: Sent %d bytes to %#x:%#x:%#x:%#x:%#x", Length, Info.MAC.Address[0], Info.MAC.Address[1], Info.MAC.Address[2], Info.MAC.Address[3], Info.MAC.Address[4], Info.MAC.Address[5]);
     }
 
-    void Ethernet::Receive(void *Data)
+    struct EthernetEventHelperStruct
+    {
+        EthernetEvents *Ptr;
+        uint16_t Type;
+    };
+
+    Vector<EthernetEventHelperStruct> RegisteredEvents;
+
+    void Ethernet::Receive(uint8_t *Data, uint64_t Length)
     {
         EthernetPacket *Packet = (EthernetPacket *)Data;
+        MediaAccessControl SourceMAC;
+        SourceMAC.FromHex(b48(Packet->Header.SourceMAC));
+        MediaAccessControl DestinationMAC;
+        DestinationMAC.FromHex(b48(Packet->Header.DestinationMAC));
 
-        if (!CompareMAC(Packet->Header.DestinationMAC, Interface->MAC) &&
-            !CompareMAC(Packet->Header.DestinationMAC, {.Address = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}}))
+        if (b48(Packet->Header.DestinationMAC) == 0xFFFFFFFFFFFF ||
+            b48(Packet->Header.DestinationMAC) == this->Interface->MAC)
         {
-            netdbg("ETH: Received data from %#x:%#x:%#x:%#x:%#x:%#x", Packet->Header.SourceMAC.Address[0], Packet->Header.SourceMAC.Address[1], Packet->Header.SourceMAC.Address[2], Packet->Header.SourceMAC.Address[3], Packet->Header.SourceMAC.Address[4], Packet->Header.SourceMAC.Address[5]);
-            switch (Packet->Header.Type)
+
+            netdbg("ETH: Received data from %02x:%02x:%02x:%02x:%02x:%02x [Type %#x]",
+                   SourceMAC.Address[0], SourceMAC.Address[1], SourceMAC.Address[2],
+                   SourceMAC.Address[3], SourceMAC.Address[4], SourceMAC.Address[5], b16(Packet->Header.Type));
+
+            bool Reply = false;
+
+            switch (b16(Packet->Header.Type))
             {
-            case ETYPE_IPV4:
-                // TODO: NetworkIPv4::IPv4(Interface).Receive(Packet->Data);
-                fixme("IPv4 not implemented");
+            case TYPE_IPV4:
+                foreach (auto var in RegisteredEvents)
+                    if (var.Type == TYPE_IPV4)
+                        Reply = var.Ptr->OnEthernetPacketReceived((uint8_t *)Packet->Data, Length);
                 break;
-            case ETYPE_ARP:
-                NetworkARP::ARP(Interface).Receive(Packet->Data);
+            case TYPE_ARP:
+                foreach (auto var in RegisteredEvents)
+                    if (var.Type == TYPE_ARP)
+                        Reply = var.Ptr->OnEthernetPacketReceived((uint8_t *)Packet->Data, Length);
                 break;
-            case ETYPE_RARP:
-                /* TODO: https://en.wikipedia.org/wiki/Reverse_Address_Resolution_Protocol */
-                // NetworkRARP::RARP(Interface).Receive(Packet->Data);
-                fixme("RARP not implemented");
+            case TYPE_RARP:
+                foreach (auto var in RegisteredEvents)
+                    if (var.Type == TYPE_RARP)
+                        Reply = var.Ptr->OnEthernetPacketReceived((uint8_t *)Packet->Data, Length);
                 break;
-            case ETYPE_IPV6:
-                // TODO: NetworkIPv6::IPv6(Interface).Receive(Packet->Data);
-                fixme("IPv6 not implemented");
+            case TYPE_IPV6:
+                foreach (auto var in RegisteredEvents)
+                    if (var.Type == TYPE_IPV6)
+                        Reply = var.Ptr->OnEthernetPacketReceived((uint8_t *)Packet->Data, Length);
                 break;
             default:
                 warn("ETH: Unknown packet type %#lx", Packet->Header.Type);
                 break;
             }
+            if (Reply)
+            {
+                Packet->Header.DestinationMAC = Packet->Header.SourceMAC;
+                Packet->Header.SourceMAC = b48(this->Interface->MAC.ToHex());
+            }
         }
         else
         {
-            warn("ETH: Invalid destination MAC %#x:%#x:%#x:%#x:%#x:%#x", Packet->Header.DestinationMAC.Address[0], Packet->Header.DestinationMAC.Address[1], Packet->Header.DestinationMAC.Address[2], Packet->Header.DestinationMAC.Address[3], Packet->Header.DestinationMAC.Address[4], Packet->Header.DestinationMAC.Address[5]);
+            netdbg("ETH: Type: [%#x] [%02x:%02x:%02x:%02x:%02x:%02x]=>[%02x:%02x:%02x:%02x:%02x:%02x]", b16(Packet->Header.Type),
+                   SourceMAC.Address[0], SourceMAC.Address[1], SourceMAC.Address[2],
+                   SourceMAC.Address[3], SourceMAC.Address[4], SourceMAC.Address[5],
+                   DestinationMAC.Address[0], DestinationMAC.Address[1], DestinationMAC.Address[2],
+                   DestinationMAC.Address[3], DestinationMAC.Address[4], DestinationMAC.Address[5]);
         }
+    }
+
+    void Ethernet::OnInterfaceReceived(NetworkInterfaceManager::DeviceInterface *Interface, uint8_t *Data, uint64_t Length)
+    {
+        if (Interface == this->Interface)
+            this->Receive(Data, Length);
+    }
+
+    EthernetEvents::EthernetEvents(FrameType Type)
+    {
+        FType = Type;
+        RegisteredEvents.push_back({.Ptr = this, .Type = (uint16_t)Type});
+    }
+
+    EthernetEvents::~EthernetEvents()
+    {
+        for (uint64_t i = 0; i < RegisteredEvents.size(); i++)
+            if (RegisteredEvents[i].Ptr == this)
+            {
+                RegisteredEvents.remove(i);
+                return;
+            }
     }
 }
