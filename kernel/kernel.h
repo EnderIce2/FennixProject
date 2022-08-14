cb0e7a4b07a36fda4bdb28cbfb8a4848a3750338
Alex <enderice2@users.noreply.github.com>
2022-08-14 19:59:18 +0300

    Updated a few files

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -1,14 +1,3 @@
 #pragma once
-#include <types.h>
-#include <boot/gbp.h>
-#include <heap.h>
-#include "boot/stivale2.h"
-
-extern uint8_t kernel_stack[STACK_SIZE];
-
-extern GlobalBootParams earlyparams;
-extern GlobalBootParams *bootparams;
-
-EXTERNC void stivale2_initializator(struct stivale2_struct *bootloaderdata);
 
 void KernelTask();
