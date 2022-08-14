8261bad612aaa3cf1c406b88097ff66cc897a866
Alex <enderice2@users.noreply.github.com>
2022-08-14 21:55:45 +0300

    Fixed compilation error (again and i hope this is the last time)

--- kernel/Kernel.cpp
+++ kernel/Kernel.cpp
@@ -3,5 +3,30 @@
 extern "C" void kernel_entry(void *data)
 {
     while (1)
-        asm("hlt");
+        asm volatile("hlt");
+}
+
+/* ---- STACK GUARD ---- */
+
+#ifndef STACK_CHK_GUARD_VALUE
+#if __UINTPTR_MAX__ == __UINT32_MAX__
+#define STACK_CHK_GUARD_VALUE 0x25F6CC8D
+#else
+#define STACK_CHK_GUARD_VALUE 0xBADFE2EC255A8572
+#endif
+#endif
+
+__attribute__((weak)) __UINTPTR_TYPE__ __stack_chk_guard = 0;
+__attribute__((weak)) __UINTPTR_TYPE__ __stack_chk_guard_init(void) { return STACK_CHK_GUARD_VALUE; }
+
+static void __attribute__((constructor, no_stack_protector)) __construct_stk_chk_guard()
+{
+    if (__stack_chk_guard == 0)
+        __stack_chk_guard = __stack_chk_guard_init();
+}
+
+__attribute__((weak, noreturn)) void __stack_chk_fail(void)
+{
+    while (1)
+        asm volatile("hlt");
 }
