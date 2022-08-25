3d95352efbda4b5f8960c3cecb95e72f2ac87dbb
Alex <enderice2@users.noreply.github.com>
2022-08-25 22:25:41 +0300

    kernel: ubsan stub

---
+++ kernel/ubsan.c
@@ -0,0 +1,17 @@
+void __ubsan_handle_type_mismatch_v1(void *type_mismatch, unsigned long pointer) {}
+void __ubsan_handle_add_overflow(void *data) {}
+void __ubsan_handle_sub_overflow(void *data) {}
+void __ubsan_handle_mul_overflow(void *data) {}
+void __ubsan_handle_divrem_overflow(void *data) {}
+void __ubsan_handle_negate_overflow(void *data) {}
+void __ubsan_handle_pointer_overflow(void *data) {}
+void __ubsan_handle_shift_out_of_bounds(void *data) {}
+void __ubsan_handle_load_invalid_value(void *data) {}
+void __ubsan_handle_out_of_bounds(void *data) {}
+void __ubsan_handle_vla_bound_not_positive(void *data) {}
+void __ubsan_handle_nonnull_return(void *data) {}
+void __ubsan_handle_nonnull_return_v1(void *data) {}
+void __ubsan_handle_nonnull_arg(void *data) {}
+void __ubsan_handle_builtin_unreachable(void *data) {}
+void __ubsan_handle_invalid_builtin(void *data) {}
+void __ubsan_handle_missing_return(void *data) {}
