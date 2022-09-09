ab1b3fa01c6f6feeb83ef9c2d970037e04054c3b
Alex <enderice2@users.noreply.github.com>
2022-09-09 05:21:19 +0300

    kernel: implemented GetError and SetError

---
+++ kernel/ErrorHandler.cpp
@@ -0,0 +1,15 @@
+#include <errorhnd.h>
+
+#include "cpu/smp.hpp"
+
+long SetError(long ErrorCode)
+{
+    debug("Setting error code to %d", ErrorCode);
+    return CurrentCPU->ErrorCode = ErrorCode;
+}
+
+long GetError()
+{
+    debug("Getting error code %d", CurrentCPU->ErrorCode);
+    return CurrentCPU->ErrorCode;
+}
